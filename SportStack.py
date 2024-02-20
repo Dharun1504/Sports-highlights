73ae720b53af2e1b14605027a3e8ea5825910ede
demi2k-sudo <85375873+demi2k-sudo@users.noreply.github.com>
2024-02-21 00:44:35 +0530

    gemini-updates

--- SportStack.py
+++ SportStack.py
@@ -1,14 +1,30 @@
-from Timestamps import Generator
+from Timestamps import Generator_gpt
 import json
 from videoGen.videoGenerate import cut_video
 import datetime
+from Timestamps import Generator_gemini
 
-
-class Highlights:
+class Highlights_gpt:
 
     def __init__(self, destination, api_base, api_version, api_key):
         self.destination = destination
-        self.gen = Generator(api_base, api_version, api_key)
+        self.gen = Generator_gpt(api_base, api_version, api_key)
+
+    def generate_highlights(self, in_path):
+        # in_path="F:\Software-Project\Highlights\Sports-highlights\Cricket-data\Sharma Stars In Thriller   SUPER OVER REPLAY   BLACKCAPS v India - 3rd T20, 2020.mp4"
+        timestamps = self.gen.get_highlight_timestamps(in_path)
+        print(timestamps)
+        j = json.loads(timestamps)
+        print(j)
+        op = f'{self.destination}{datetime.datetime.now().timestamp()}.mp4'
+        cut_video(in_path, op, j)
+        return op
+    
+class Highlights_gemini:
+
+    def __init__(self, destination,api_key):
+        self.destination = destination
+        self.gen = Generator_gemini(api_key)
 
     def generate_highlights(self, in_path):
         # in_path="F:\Software-Project\Highlights\Sports-highlights\Cricket-data\Sharma Stars In Thriller   SUPER OVER REPLAY   BLACKCAPS v India - 3rd T20, 2020.mp4"
--- Timestamps.py
+++ Timestamps.py
@@ -2,8 +2,9 @@ from Whisper import transcription
 from VidToAudio import vidAud
 from LLM import gpt
 import datetime
+from LLM import gemini
 
-class Generator:
+class Generator_gpt:
     def __init__(self,api_base, api_version, api_key):
         self.api_base = api_base
         self.api_version = api_version
@@ -26,13 +27,36 @@ class Generator:
         completion = self.llm.get_timestamps(transcripts)
         print("\n\n",completion["choices"][0]["message"]["content"],"\n\n")
         return completion["choices"][0]["message"]["content"]
+  
+class Generator_gemini:
+    def __init__(self,api_key):
+        self.api_key = api_key
+        self.wav_path = "F:\Software-Project\Highlights\Sports-highlights\Audios\\"
+        self.conv = vidAud.Convertor()
+        self.trans = transcription.Transcriptor()
+        self.llm = gemini.LLM_gemini(api_key)
         
+    def get_highlight_timestamps(self,input_video_path):
+        output_wav_path = f'{self.wav_path}{datetime.datetime.now().timestamp()}.wav'
+        print('This is the path :',output_wav_path)
+        self.conv.to_wav(input_video_path, output_wav_path)
+        print(f"Conversion complete. WAV file saved at: {output_wav_path}")
+        
+        transcripts = self.trans.transcribe(output_wav_path)
+        print(transcripts)
+        print("Transcription done.. Sending to LLM")
+        
+        completion = self.llm.get_timestamps(transcripts)
+        print("\n\n",completion,"\n\n")
+        completion = completion.replace('(','[')
+        completion = completion.replace(')',']')
+        return completion
 
 if __name__=='main':
 
     input_video_path = "F:\Software-Project\Sport-Highlights\Cricket-data\DRAMATIC Final Over!   15 Runs To Win Off 6 Balls   West Indies v India   1st CG United ODI 2022.mp4"
 
-    gen = Generator()
+    gen = Generator_gpt()
     completion = gen.get_highlight_timestamps(input_video_path)
 
     print(completion)
